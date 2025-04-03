7ddd856a117b8730d091ccdeeea0a609bc8199c5
C. M. Frederiksen <150817279+CasperTheFriendlyCodeWizard@users.noreply.github.com>
2025-04-03 13:30:59 +0200

    Update MALT_IPI.R
    By Christian Brieghel

--- dalycare/functions/MALT_IPI.R
+++ dalycare/functions/MALT_IPI.R
@@ -15,10 +15,10 @@ MALT_IPI = function(data){
   print_color('\nThieblemont et al. Blood. 2017;130(12):1409-1417.\n', 'black')
   data %>% 
     mutate(Age.score = ifelse(age_diagnosis < 75, 0, 1),
-           LDH.score = as.numeric(as.character(LDH_elevated_diagnosis)),
+           LDH.score = ifelse(LDH_elevated_diagnosis=='No', 0, 1),
            STAGE.score = ifelse(as.numeric(as.character(AA_stage_diagnosis)) <3, 0, 1)) %>% 
     mutate(MALT_IPI.score = rowSums(across(Age.score:STAGE.score)),
-           MALT_IPI.score = ifelse(subtype == 'WM', MALT_IPI.score, NA)) %>% 
+           MALT_IPI.score = ifelse(subtype == 'MZL', MALT_IPI.score, NA)) %>% 
     mutate(MALT_IPI = cut(MALT_IPI.score, c(-Inf, 0, 1, Inf), labels =  c('Low', 'Intermediate', 'High')))
 } 
 
