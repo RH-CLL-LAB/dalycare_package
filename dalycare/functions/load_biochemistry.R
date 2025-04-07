3f2c67c8143d5d6434fd04e97553e501d49f2356
C. M. Frederiksen <150817279+CasperTheFriendlyCodeWizard@users.noreply.github.com>
2025-04-07 10:05:15 +0200

    Update load_biochemistry.R
    Changed source table

--- dalycare/functions/load_biochemistry.R
+++ dalycare/functions/load_biochemistry.R
@@ -4,11 +4,11 @@ load_biochemistry = function(labs = NULL, assign = FALSE){
   #' @author
   #' christian brieghel
   #' @description
-  #' Loads dataset containing biochemistry from SDS_lab_forsker.
+  #' Loads dataset containing biochemistry from laboratorymeasurements
   #' 'labs' must contain NPU codes, e.g. from lists from load_npu_common()
   #' @examples
   #' LAB_df = load_biochemistry(c(NPU.B2M, NPU.LDH))
-  #' BSI_df = load_biochemistry(NPU.BSI) #Blood cultures # assigns data as SDS_lab_forsker_subset into Global Environment
+  #' BSI_df = load_biochemistry(NPU.BSI) #Blood cultures # assigns data as laboratorymeasurements_subset into Global Environment
   #' load_biochemistry(labs = NPU.GROUP.MSPIKE, assign = TRUE)
   
   un <- Sys.info()[['user']]
@@ -16,19 +16,19 @@ load_biochemistry = function(labs = NULL, assign = FALSE){
   
   
   pg <- dbConnect(RPostgres::Postgres(),
-                  dbname = 'import', 
+                  dbname = 'core', 
                   host = 'localhost',
                   port = 5432,
                   user = un,
                   password = pw,
                   options="-c search_path=public")
-  ENTER = paste0('select * from "', 'SDS_lab_forsker','" where ',
+  ENTER = paste0('select * from "', 'laboratorymeasurements','" where ',
                  'analysiscode',
                  ' in (',
                  paste0(paste0('\'', paste({{labs}}, collapse = '\', \'')), '\''),
                  ')')
   if (assign) {
-    assign('SDS_lab_forsker_subset',  dbGetQuery(pg, ENTER), envir = parent.frame())
+    assign('laboratorymeasurements_subset',  dbGetQuery(pg, ENTER), envir = parent.frame())
   }else{
     return(dbGetQuery(pg, ENTER))
   }
